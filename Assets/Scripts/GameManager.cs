738fdf07870a71ac75c0b6dce463672d18335315
Bibuba <princesakenny98@gmail.com>
2025-10-05 17:39:02 +0300

    transition

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// Рутовый менеджер игры, который сохраняется между сценами
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
